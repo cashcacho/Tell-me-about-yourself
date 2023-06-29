2573dad0fed3ec7ab1d5febb2105b5c70ac4af9d
Justine Cacho <justinecacho@gmail.com>
2023-06-28 22:28:02 -0700

    updates index

---
+++ script.js
@@ -0,0 +1,4 @@
+var firstName = "Justine";
+var lastName = "Cacho";
+var yearsOfStudy = "1";
+var goal = "To land a job within a Fortune 500 company in order to receive a good salary, benefits, and health insurance. Also, by obtaining a salary I'll be able to grow my current business as well as have constany money flowing in for investments and risk assets.";
